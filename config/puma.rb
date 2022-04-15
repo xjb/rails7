19c481ca331b0e8b6ca9fa4172348c9b9ebe052b
xjb.laptop <xjbwks+laptop@gmail.com>
2022-04-15 22:19:01 +0900

    Enable SSL on development

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT", 3000), "0.0.0.0"
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,3 +41,12 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# Enable SSL
+require "localhost/authority"
+authority = Localhost::Authority.fetch
+
+ssl_bind "0.0.0.0", "3443", {
+  key: authority.key_path,
+  cert: authority.certificate_path
+}
